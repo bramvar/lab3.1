5d37e736d4f26b8a899025fb363e84efd7bdf80a
vardo159 <braianelsociales@gmail.com>
2019-04-18 23:33:11 -0500

    update

--- structu/vrAVLTree/VrAvlTree.java
+++ structu/vrAVLTree/VrAvlTree.java
@@ -1,5 +1,64 @@
 package vrAVLTree;
 
-public class VrAvlTree {
+import exceptions.ElementException;
+import interfaces.IvrTreeO;
 
+public class VrAvlTree<T extends Comparable<? super T>> implements IvrTreeO<T>{
+
+	private VrAvlTreeNode<T> root;
+	private int weight;
+	
+	public  VrAvlTree() {
+		root=null;
+		weight=0;
+	}
+	
+	public  VrAvlTree(VrAvlTreeNode<T> n,int p) {
+		root=n;
+		weight=p;
+	}
+	
+	
+	@Override
+	public int getHeight() {
+		return ( root != null ) ? root.getHeight() : 0;
+	}
+
+	@Override
+	public int getWeight() {
+		return weight;
+	}
+
+	@Override
+	public T search(T e) {
+		return ( root != null ) ? root.search(e) : null;
+	}
+
+	@Override
+	public void insert(T elem) throws ElementException {
+		if( root == null ){
+			root = new VrAvlTreeNode<T>( elem );
+		}
+		else{
+			root = root.insert( elem );
+		}
+		weight++;
+	}
+
+	@Override
+	public void delete(T elem) throws ElementException {
+		if( root != null ){
+			root = root.delete( elem );
+			weight--;
+		}
+		else	{
+			throw new ElementException(ElementException.NO_SUCH_ELEMENT);
+		}
+		
+	}
+
+	
+
+	
+	
 }
